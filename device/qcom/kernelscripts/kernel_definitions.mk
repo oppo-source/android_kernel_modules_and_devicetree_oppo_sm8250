fbb66cf5c458ca4e1deb91ee4524fb9309408dab
pswbuild <pswbuild@oppo.com>
2024-11-29 10:28:21 +0800

    Synchronize code for OPPO OPD2101_14.0.0.1200(CN01) Based on QCOM release TAG: kernel/build:r_sm8250_08.24.3_20200825 kernel/devicetree-4.19:AU_LINUX_ANDROID_LA.UM.9.12.R1.11.00.00.597.135.01_AU_LINUX_ANDROID_LA.QSSI.11.0.R1.11.00.00.668.141.00_r00028.1 platform/vendor/opensource/audio-kernel:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009 platform/vendor/opensource/camera-kernel:r_sm8250_08.24.3_20200825 platform/vendor/opensource/display-drivers:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009 platform/vendor/opensource/video-driver:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009 platform/vendor/qcom-opensource/data-kernel:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009 platform/vendor/qcom-opensource/wlan/fw-api:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009 platform/vendor/qcom-opensource/wlan/qcacld-3.0:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009 platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009 platform/vendor/qcom-opensource/wlan/utils/sigma-dut:AU_LINUX_ANDROID_LA.UM.9.12.1.R1.11.00.00.893.009

---
+++ device/qcom/kernelscripts/kernel_definitions.mk
@@ -0,0 +1,275 @@
+# Android Kernel compilation/common definitions
+ifeq ($(TARGET_BUILD_VARIANT),user)
+     KERNEL_DEFCONFIG ?= vendor/$(TARGET_BOARD_PLATFORM)-perf_defconfig
+endif
+
+#ifdefVENDOR_EDIT
+#ifeq ($(KERNEL_DEFCONFIG),)
+#     KERNEL_DEFCONFIG := vendor/$(TARGET_PRODUCT)_defconfig
+#endif
+#else
+ifeq ($(KERNEL_DEFCONFIG),)
+   ifeq ($(TARGET_BUILD_VARIANT),user)
+     KERNEL_DEFCONFIG := vendor/$(TARGET_PRODUCT)-perf_defconfig
+   else
+     KERNEL_DEFCONFIG := vendor/$(TARGET_PRODUCT)_defconfig
+   endif
+endif
+#endif
+
+TARGET_KERNEL := msm-$(TARGET_KERNEL_VERSION)
+ifeq ($(TARGET_KERNEL_SOURCE),)
+     TARGET_KERNEL_SOURCE := kernel/$(TARGET_KERNEL)
+endif
+
+DTC := $(HOST_OUT_EXECUTABLES)/dtc$(HOST_EXECUTABLE_SUFFIX)
+UFDT_APPLY_OVERLAY := $(HOST_OUT_EXECUTABLES)/ufdt_apply_overlay$(HOST_EXECUTABLE_SUFFIX)
+
+SOURCE_ROOT := $(shell pwd)
+KERNEL_SRC_DEP := $(shell find $(SOURCE_ROOT)/$(TARGET_KERNEL_SOURCE)/ -type f -name "*")
+KERNEL_SRC_DEP += $(foreach item, $(shell find $(SOURCE_ROOT)/$(TARGET_KERNEL_SOURCE)/ -type l -not -path "*/.git/*"|xargs realpath|grep -v "kernel\/$(TARGET_KERNEL)"), $(shell find ${item} -name "*" -type f -not -path "*/.git/*" 2>/dev/null))
+TARGET_KERNEL_MAKE_ENV := DTC_EXT=$(SOURCE_ROOT)/$(DTC)
+TARGET_KERNEL_MAKE_ENV += DTC_OVERLAY_TEST_EXT=$(SOURCE_ROOT)/$(UFDT_APPLY_OVERLAY)
+ifeq ($(BOARD_KERNEL_SEPARATED_DTBO), true)
+TARGET_KERNEL_MAKE_ENV += CONFIG_BUILD_ARM64_DT_OVERLAY=y
+endif
+TARGET_KERNEL_MAKE_ENV += HOSTCC=$(SOURCE_ROOT)/$(SOONG_LLVM_PREBUILTS_PATH)/clang
+TARGET_KERNEL_MAKE_ENV += HOSTAR=$(SOURCE_ROOT)/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/bin/x86_64-linux-ar
+TARGET_KERNEL_MAKE_ENV += HOSTLD=$(SOURCE_ROOT)/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/bin/x86_64-linux-ld
+TARGET_KERNEL_MAKE_ENV += M4=$(SOURCE_ROOT)/prebuilts/build-tools/linux-x86/bin/m4
+TARGET_KERNEL_MAKE_ENV += LEX=$(SOURCE_ROOT)/prebuilts/build-tools/linux-x86/bin/flex
+TARGET_KERNEL_MAKE_ENV += YACC=$(SOURCE_ROOT)/prebuilts/build-tools/linux-x86/bin/bison
+TARGET_KERNEL_MAKE_CFLAGS = "-I$(SOURCE_ROOT)/$(TARGET_KERNEL_SOURCE)/include/uapi -I/usr/include -I/usr/include/x86_64-linux-gnu -L/usr/lib -L/usr/lib/x86_64-linux-gnu -fuse-ld=lld"
+TARGET_KERNEL_MAKE_LDFLAGS = "-L/usr/lib -L/usr/lib/x86_64-linux-gnu -fuse-ld=lld"
+
+KERNEL_LLVM_BIN := $(lastword $(sort $(wildcard $(SOURCE_ROOT)/$(LLVM_PREBUILTS_BASE)/$(BUILD_OS)-x86/clang-4*)))/bin/clang
+
+KERNEL_TARGET := $(strip $(INSTALLED_KERNEL_TARGET))
+ifeq ($(KERNEL_TARGET),)
+INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
+endif
+
+ifneq ($(TARGET_KERNEL_APPEND_DTB), true)
+$(info Using DTB Image)
+INSTALLED_DTBIMAGE_TARGET := $(PRODUCT_OUT)/dtb.img
+endif
+
+TARGET_KERNEL_ARCH := $(strip $(TARGET_KERNEL_ARCH))
+ifeq ($(TARGET_KERNEL_ARCH),)
+KERNEL_ARCH := arm
+else
+KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
+endif
+
+ifeq ($(shell echo $(KERNEL_DEFCONFIG) | grep vendor),)
+KERNEL_DEFCONFIG := vendor/$(KERNEL_DEFCONFIG)
+endif
+
+# Force 32-bit binder IPC for 64bit kernel with 32bit userspace
+ifeq ($(KERNEL_ARCH),arm64)
+ifeq ($(TARGET_ARCH),arm)
+KERNEL_CONFIG_OVERRIDE := CONFIG_ANDROID_BINDER_IPC_32BIT=y
+endif
+endif
+
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(strip $(TARGET_KERNEL_CROSS_COMPILE_PREFIX))
+ifeq ($(TARGET_KERNEL_CROSS_COMPILE_PREFIX),)
+KERNEL_CROSS_COMPILE := arm-eabi-
+else
+KERNEL_CROSS_COMPILE := $(TARGET_KERNEL_CROSS_COMPILE_PREFIX)
+endif
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+
+KERNEL_GCC_NOANDROID_CHK := $(shell (echo "int main() {return 0;}" | $(KERNEL_CROSS_COMPILE)gcc -E -mno-android - > /dev/null 2>&1 ; echo $$?))
+
+real_cc :=
+ifeq ($(KERNEL_LLVM_SUPPORT),true)
+  ifeq ($(KERNEL_SD_LLVM_SUPPORT), true)  #Using sd-llvm compiler
+    ifeq ($(shell echo $(SDCLANG_PATH) | head -c 1),/)
+       KERNEL_LLVM_BIN := $(SDCLANG_PATH)/clang
+    else
+       KERNEL_LLVM_BIN := $(shell pwd)/$(SDCLANG_PATH)/clang
+    endif
+    $(warning "Using sdllvm" $(KERNEL_LLVM_BIN))
+  else
+    KERNEL_LLVM_BIN := $(shell pwd)/$(CLANG) #Using aosp-llvm compiler
+    $(warning "Using aosp-llvm" $(KERNEL_LLVM_BIN))
+  endif
+  ifeq ($(KERNEL_ARCH), arm64)
+      real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=aarch64-linux-gnu-
+  else
+      real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=arm-linux-gnueabihf
+  endif
+else
+ifeq ($(strip $(KERNEL_GCC_NOANDROID_CHK)),0)
+KERNEL_CFLAGS := KCFLAGS=-mno-android
+endif
+endif
+
+BUILD_ROOT_LOC := ../../..
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/kernel/$(TARGET_KERNEL)
+KERNEL_SYMLINK := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+KERNEL_USR := $(KERNEL_SYMLINK)/usr
+
+KERNEL_CONFIG := $(KERNEL_OUT)/.config
+
+ifeq ($(KERNEL_DEFCONFIG)$(wildcard $(KERNEL_CONFIG)),)
+$(error Kernel configuration not defined, cannot build kernel)
+else
+
+TARGET_USES_UNCOMPRESSED_KERNEL := $(shell grep "CONFIG_BUILD_ARM64_UNCOMPRESSED_KERNEL=y" $(TARGET_KERNEL_SOURCE)/arch/arm64/configs/$(KERNEL_DEFCONFIG))
+
+ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),)
+ifeq ($(KERNEL_ARCH),arm64)
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image.gz
+else
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/zImage
+endif
+else
+$(info Using uncompressed kernel)
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/Image
+endif
+
+ifeq ($(TARGET_KERNEL_APPEND_DTB), true)
+$(info Using appended DTB)
+TARGET_PREBUILT_INT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)-dtb
+endif
+
+KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
+KERNEL_MODULES_INSTALL ?= system
+KERNEL_MODULES_OUT ?= $(PRODUCT_OUT)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
+TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
+
+endif
+endif
+
+# Add RTIC DTB to dtb.img if RTIC MPGen is enabled.
+# Note: unfortunately we can't define RTIC DTS + DTB rule here as the
+# following variable/ tools (needed for DTS generation)
+# are missing - DTB_OBJS, OBJDUMP, KCONFIG_CONFIG, CC, DTC_FLAGS (the only available is DTC).
+# The existing RTIC kernel integration in scripts/link-vmlinux.sh generates RTIC MP DTS
+# that will be compiled with optional rule below.
+# To be safe, we check for MPGen enable.
+ifdef RTIC_MPGEN
+RTIC_DTB := $(KERNEL_SYMLINK)/rtic_mp.dtb
+endif
+
+MAKE_PATH := $(SOURCE_ROOT)/prebuilts/build-tools/linux-x86/bin/
+# Android Kernel make rules
+
+#ifdef COLOR_EDIT
+my_feature_file := $(SOURCE_ROOT)/kernel/oplus_native_features.sh
+
+$(shell echo "#!/bin/bash" > $(my_feature_file))
+$(shell echo 'export OPLUS_NATIVE_FEATURE_SET="$(strip $(SOONG_CONFIG_oplusNativeFeaturePlugin))"' >> $(my_feature_file))
+$(foreach key,$(SOONG_CONFIG_oplusNativeFeaturePlugin), \
+  $(shell echo 'export $(key)=$(SOONG_CONFIG_oplusNativeFeaturePlugin_$(key))' >> $(my_feature_file))\
+)
+#endif
+
+#ifndef OPLUS_FEATURE_FORCE_SELINUX
+#$(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT) $(DTC) $(UFDT_APPLY_OVERLAY)
+#	KERNEL_DIR=$(TARGET_KERNEL_SOURCE) \
+#	DEFCONFIG=$(KERNEL_DEFCONFIG) \
+#	OUT_DIR=$(KERNEL_OUT) \
+#	MAKE_PATH=$(MAKE_PATH)\
+#	ARCH=$(KERNEL_ARCH) \
+#	CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) \
+#	KERNEL_MODULES_OUT=$(KERNEL_MODULES_OUT) \
+#	KERNEL_HEADERS_INSTALL=$(KERNEL_HEADERS_INSTALL) \
+#	HEADERS_INSTALL=1 \
+#	TARGET_PREBUILT_INT_KERNEL=$(TARGET_PREBUILT_INT_KERNEL) \
+#	AGING_DEBUG_MASK=$(AGING_DEBUG_MASK) \
+#	TARGET_INCLUDES=$(TARGET_KERNEL_MAKE_CFLAGS) \
+#	TARGET_LINCLUDES=$(TARGET_KERNEL_MAKE_LDFLAGS) \
+#	TARGET_MEMLEAK_DETECT_TEST=$(TARGET_MEMLEAK_DETECT_TEST) \
+#	device/qcom/kernelscripts/buildkernel.sh \
+#	$(real_cc) \
+#	$(TARGET_KERNEL_MAKE_ENV)
+#else OPLUS_FEATURE_FORCE_SELINUX
+$(KERNEL_HEADERS_INSTALL): $(DTC) $(UFDT_APPLY_OVERLAY) | $(KERNEL_OUT)
+	KERNEL_DIR=$(TARGET_KERNEL_SOURCE) \
+	DEFCONFIG=$(KERNEL_DEFCONFIG) \
+	OUT_DIR=$(KERNEL_OUT) \
+	MAKE_PATH=$(MAKE_PATH)\
+	ARCH=$(KERNEL_ARCH) \
+	CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) \
+	KERNEL_MODULES_OUT=$(KERNEL_MODULES_OUT) \
+	KERNEL_HEADERS_INSTALL=$(KERNEL_HEADERS_INSTALL) \
+	HEADERS_INSTALL=1 \
+	TARGET_PREBUILT_INT_KERNEL=$(TARGET_PREBUILT_INT_KERNEL) \
+	AGING_DEBUG_MASK=$(AGING_DEBUG_MASK) \
+	TARGET_INCLUDES=$(TARGET_KERNEL_MAKE_CFLAGS) \
+	TARGET_LINCLUDES=$(TARGET_KERNEL_MAKE_LDFLAGS) \
+	OBSOLETE_KEEP_ADB_SECURE=$(OBSOLETE_KEEP_ADB_SECURE) \
+	TARGET_MEMLEAK_DETECT_TEST=$(TARGET_MEMLEAK_DETECT_TEST) \
+	device/qcom/kernelscripts/buildkernel.sh \
+	$(real_cc) \
+	$(TARGET_KERNEL_MAKE_ENV)
+#endif OPLUS_FEATURE_FORCE_SELINUX
+
+$(KERNEL_OUT):
+	mkdir -p $(KERNEL_OUT)
+
+$(KERNEL_USR): $(KERNEL_HEADERS_INSTALL)
+	rm -rf $(KERNEL_SYMLINK)
+	ln -s kernel/$(TARGET_KERNEL) $(KERNEL_SYMLINK)
+
+#ifndef OPLUS_FEATURE_FORCE_SELINUX
+#$(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(DTC) $(KERNEL_USR)
+#	KERNEL_DIR=$(TARGET_KERNEL_SOURCE) \
+#	DEFCONFIG=$(KERNEL_DEFCONFIG) \
+#	OUT_DIR=$(KERNEL_OUT) \
+#	ARCH=$(KERNEL_ARCH) \
+#	CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) \
+#	KERNEL_MODULES_OUT=$(KERNEL_MODULES_OUT) \
+#	KERNEL_HEADERS_INSTALL=$(KERNEL_HEADERS_INSTALL) \
+#	TARGET_PREBUILT_INT_KERNEL=$(TARGET_PREBUILT_INT_KERNEL) \
+#	AGING_DEBUG_MASK=$(AGING_DEBUG_MASK) \
+#	TARGET_INCLUDES=$(TARGET_KERNEL_MAKE_CFLAGS) \
+#	TARGET_LINCLUDES=$(TARGET_KERNEL_MAKE_LDFLAGS) \
+#	TARGET_MEMLEAK_DETECT_TEST=$(TARGET_MEMLEAK_DETECT_TEST) \
+#	device/qcom/kernelscripts/buildkernel.sh \
+#	$(real_cc) \
+#	$(TARGET_KERNEL_MAKE_ENV)
+#else OPLUS_FEATURE_FORCE_SELINUX
+$(TARGET_PREBUILT_KERNEL): $(DTC) $(KERNEL_USR) $(KERNEL_SRC_DEP) | $(KERNEL_OUT)
+	KERNEL_DIR=$(TARGET_KERNEL_SOURCE) \
+	DEFCONFIG=$(KERNEL_DEFCONFIG) \
+	OUT_DIR=$(KERNEL_OUT) \
+	MAKE_PATH=$(MAKE_PATH) \
+	ARCH=$(KERNEL_ARCH) \
+	CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) \
+	KERNEL_MODULES_OUT=$(KERNEL_MODULES_OUT) \
+	KERNEL_HEADERS_INSTALL=$(KERNEL_HEADERS_INSTALL) \
+	TARGET_PREBUILT_INT_KERNEL=$(TARGET_PREBUILT_INT_KERNEL) \
+	AGING_DEBUG_MASK=$(AGING_DEBUG_MASK) \
+	TARGET_INCLUDES=$(TARGET_KERNEL_MAKE_CFLAGS) \
+	TARGET_LINCLUDES=$(TARGET_KERNEL_MAKE_LDFLAGS) \
+	OBSOLETE_KEEP_ADB_SECURE=$(OBSOLETE_KEEP_ADB_SECURE) \
+	TARGET_MEMLEAK_DETECT_TEST=$(TARGET_MEMLEAK_DETECT_TEST) \
+	device/qcom/kernelscripts/buildkernel.sh \
+	$(real_cc) \
+	$(TARGET_KERNEL_MAKE_ENV)
+#endif OPLUS_FEATURE_FORCE_SELINUX
+
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+# RTIC DTS to DTB (if MPGen enabled;
+# and make sure we don't break the build if rtic_mp.dts missing)
+$(RTIC_DTB): $(INSTALLED_KERNEL_TARGET)
+	stat $(KERNEL_SYMLINK)/rtic_mp.dts 2>/dev/null >&2 && \
+	$(DTC) -O dtb -o $(RTIC_DTB) -b 1 $(DTC_FLAGS) $(KERNEL_SYMLINK)/rtic_mp.dts || \
+	touch $(RTIC_DTB)
+
+# Creating a dtb.img once the kernel is compiled if TARGET_KERNEL_APPEND_DTB is set to be false
+#ifdef VENDOR_EDIT
+$(INSTALLED_DTBIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
+	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/*/*.dtb $(RTIC_DTB) > $@
+#else
+#$(INSTALLED_DTBIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
+#	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(RTIC_DTB) > $@
+#endif
